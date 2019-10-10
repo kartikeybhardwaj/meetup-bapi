eb249e2069212f5b22c881dde4e436dc27f37e4f
Kartikey Bhardwaj <kartikey.bh21@gmail.com>
2019-10-10 23:52:18 +0530

    added routed

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ from src.user.UserLogin import UserLogin
 from src.meetup.AddMeetup import AddMeetup
 from src.meetup.GetMeetup import GetMeetup
 from src.meetup.Register import Register
+from src.meetup.GetLiveMeetups import GetLiveMeetups
+from src.meetup.GetUpcomingMeetups import GetUpcomingMeetups
+from src.meetup.GetPreviousMeetups import GetPreviousMeetups
 from utils.Empty import Empty
 
 cors = CORS(allow_origins_list = ['http://localhost:4200'],
@@ -24,6 +27,9 @@ api.add_route('/fast-forward', Empty())
 api.add_route('/add-meetup', AddMeetup())
 api.add_route('/get-meetup', GetMeetup())
 api.add_route('/register-to-meetup', Register())
+api.add_route('/get-live-meetups', GetLiveMeetups())
+api.add_route('/get-upcoming-meetups', GetUpcomingMeetups())
+api.add_route('/get-previous-meetups', GetPreviousMeetups())
 
 # pip3 install falcon
 # pip3 install falcon-cors
